54b17093fb68ae07395699771e8b368eda62ec63
BrunoLeon007 <brunob734@gmail.com>
2024-11-25 20:35:32 -0300

    Add files via upload

--- ex004/script.js
+++ ex004/script.js
@@ -2,9 +2,9 @@ const numero = document.querySelector("#numeroUsuario")
 const btnTabuada = document.querySelector("#btntabuada")
 const resultado = document.querySelector("#res")
 
-function resultadoCalculo(){
+function resultadoCalculo(){/*primeiro passo chamar a função*/
   resultado.innerHTML = "" /* limpar tabuada implementada*/
-  var numeroConvertido = Number(numero.value)
+  var numeroConvertido = Number(numero.value)/*chamar uma var (variavel) converter o numero. valor*/
 
   var inicial = 1
 while(inicial <=10){
